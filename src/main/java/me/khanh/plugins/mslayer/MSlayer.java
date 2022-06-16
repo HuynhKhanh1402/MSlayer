a45b8644a7a85c2aa022239e5af17d59b40f2ac0
Huỳnh Khánh <khanh@duck.com>
2022-06-17 00:19:36 +0700

    Update

--- src/main/java/me/khanh/plugins/mslayer/MSlayer.java
+++ src/main/java/me/khanh/plugins/mslayer/MSlayer.java
@@ -1,19 +1,29 @@
 package me.khanh.plugins.mslayer;
 
+import lombok.Getter;
 import me.khanh.plugins.mslayer.compatibility.MythicMobsFactory;
+import me.khanh.plugins.mslayer.compatibility.QuestsFactory;
 import me.khanh.plugins.mslayer.compatibility.provided.MythicMobs;
 import me.khanh.plugins.mslayer.compatibility.provided.MythicMobsLegacy;
 import me.khanh.plugins.mslayer.utils.LoggerUtils;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public final class MSlayer extends JavaPlugin {
 
+    @Getter
     private MythicMobsFactory mythicMobsFactory;
+    @Getter
+    private QuestsFactory questsFactory;
 
     @Override
     public void onEnable() {
         loadMythicMobs();
+        questsFactory = new QuestsFactory();
+        for (String s : questsFactory.getQuests()) {
+            LoggerUtils.info(s);
+        }
     }
 
     @Override
@@ -25,14 +35,11 @@ public final class MSlayer extends JavaPlugin {
         Plugin mythicMobsPlugin = getServer().getPluginManager().getPlugin("MythicMobs");
         assert mythicMobsPlugin != null;
         int version = Integer.parseInt(mythicMobsPlugin.getDescription().getVersion().split("\\.")[0]);
-        LoggerUtils.info(String.valueOf(version));
         if (version > 4){
             mythicMobsFactory = new MythicMobs();
         } else {
             mythicMobsFactory = new MythicMobsLegacy();
         }
-        for (String name : mythicMobsFactory.getMythicMobs()){
-            LoggerUtils.info(name);
-        }
+
     }
 }
--- src/main/java/me/khanh/plugins/mslayer/compatibility/QuestsFactory.java
+++ src/main/java/me/khanh/plugins/mslayer/compatibility/QuestsFactory.java
@@ -1,4 +1,13 @@
 package me.khanh.plugins.mslayer.compatibility;
 
+import me.blackvein.quests.Quests;
+import me.blackvein.quests.quests.IQuest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QuestsFactory {
+    public List<String> getQuests() {
+        return Quests.getPlugin(Quests.class).getLoadedQuests().stream().map(IQuest::getId).collect(Collectors.toList());
+    }
 }
